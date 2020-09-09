2daa8b13578d32c5130e98fd04004ab972b3956d
Shivansh Goel <shivanshgoel009@gmail.com>
2020-09-09 17:41:37 +0530

    Update Reduce Startup Impact.bat

--- Reduce Startup Impact.bat
+++ Reduce Startup Impact.bat
@@ -1,9 +1,13 @@
 @echo off
-start "" "C:\Program Files\Google\Drive\googledrivesync.exe"
-start "" "C:\Program Files (x86)\Mouse Server\MouseServer.exe"
-start "" "C:\Users\shiva\AppData\Local\Microsoft\OneDrive\OneDrive.exe"
-timeout 2
-wscript "D:\Program Files (x86)\Drives-Toggle-Switch\Files\autokey.vbs"
-cd C:\Users\shiva\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\
+taskkill /IM googledrivesync.exe /T /F
+taskkill /IM OneDrive.exe /T /F
+taskkill /IM Chrome.exe /T /F
+taskkill /IM WhatsApp.exe /T /F
+taskkill /IM AnyDesk.exe /T /F
+taskkill /IM Zoom.exe /T /F
+taskkill /IM Bluestacks.exe /T /F
+taskkill /IM Steam.exe /T /F
+taskkill /IM EpicGamesLauncher.exe /T /F
+cd C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\
 start discord
 Exit
